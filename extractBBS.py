bc8fd4b0bbec9d4ff650bd2e77435723d9b92e1e
Jason Wang <wang.kehan@berkeley.edu>
2020-10-22 19:29:40 -0700

    update extract

--- extractBBS.py
+++ extractBBS.py
@@ -1,7 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-for idx in tqdm(range(100000)):
+import threading
+
+num_processes = 40
+tot_img = 100000
+step = tot_img // num_processes
+annotations = [""] * num_processes
+
+def extract(start_idx, end_idx, i)
+for idx in tqdm(range(start_idx, end_idx)):
     directory = "../dataset/Y/"
     imname = "train_mask_{}.png".format(idx)
     im = cv2.imread(directory + imname)
@@ -12,4 +20,20 @@ for idx in tqdm(range(100000)):
         x,y,w,h = cv2.boundingRect(cnt)
         annotation += " {},{},{},{},0".format(x, y, x+w, y+h)
     if len(contours) > 0:
-        print(annotation)
+        annotations[i] += annotation + '\n'
+
+
+if __name__ == '__main__':
+    for i in range(num_processes):
+        processes.append(threading.Thread(target=generate, args=(i*step, (i+1)*step, i,)))
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+        
+    output = ""
+    for i in range(num_processes):
+        output += annotations[i]
+    print(output)
