99eac407c5dd540396bfe62b9b4587e0a0874b9e
Jason Wang <wang.kehan@berkeley.edu>
2020-10-28 15:52:49 -0700

    change batch

--- cfg.py
+++ cfg.py
@@ -21,7 +21,7 @@ Cfg = EasyDict()
 Cfg.use_darknet_cfg = False
 Cfg.cfgfile = os.path.join(_BASE_DIR, 'cfg', 'yolov4.cfg')
 
-Cfg.batch = 32
+Cfg.batch = 16
 Cfg.subdivisions = 1
 Cfg.width = 608
 Cfg.height = 608
