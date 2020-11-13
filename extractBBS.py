4cf006b3004776126721a7b464781e276cdb3906
EC2 Default User <ec2-user@ip-172-16-181-248.ec2.internal>
2020-11-13 11:32:09 +0000

    notebook changes

--- extractBBS.py
+++ extractBBS.py
@@ -8,24 +8,25 @@ tot_img = 100000
 step = tot_img // num_processes
 annotations = [""] * num_processes
 
-def extract(start_idx, end_idx, i)
-for idx in tqdm(range(start_idx, end_idx)):
-    directory = "../dataset/Y/"
-    imname = "train_mask_{}.png".format(idx)
-    im = cv2.imread(directory + imname)
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    contours, hierarchy = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-    annotation = str(imname)
-    for cnt in contours:
-        x,y,w,h = cv2.boundingRect(cnt)
-        annotation += " {},{},{},{},0".format(x, y, x+w, y+h)
-    if len(contours) > 0:
-        annotations[i] += annotation + '\n'
+def extract(start_idx, end_idx, i):
+    for idx in tqdm(range(start_idx, end_idx)):
+        directory = "../dataset/Y/"
+        imname = "train_mask_{}.png".format(idx)
+        im = cv2.imread(directory + imname)
+        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        contours, hierarchy = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+        annotation = str(imname)
+        for cnt in contours:
+            x,y,w,h = cv2.boundingRect(cnt)
+            annotation += " {},{},{},{},0".format(x, y, x+w, y+h)
+        if len(contours) > 0:
+            annotations[i] += annotation + '\n'
 
 
 if __name__ == '__main__':
+    processes = []
     for i in range(num_processes):
-        processes.append(threading.Thread(target=generate, args=(i*step, (i+1)*step, i,)))
+        processes.append(threading.Thread(target=extract, args=(i*step, (i+1)*step, i,)))
 
     for p in processes:
         p.start()
